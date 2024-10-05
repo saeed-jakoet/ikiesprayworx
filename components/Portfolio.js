ece91526ada6322303687eddb23601573e582527
saeed-jakoet <jakoetsaeed@gmail.com>
2024-10-05 19:12:27 +0200

    gallery images

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -2,54 +2,132 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 const Portfolio = () => {
-    const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedImageIndex, setSelectedImageIndex] = useState(null);
     const [lightboxDisplay, setLightboxDisplay] = useState(false);
 
-    const handleOpenModal = (image) => {
-        setSelectedImage(image);
+    const handleOpenModal = (index) => {
+        setSelectedImageIndex(index);
         setLightboxDisplay(true);
         document.body.style.overflow = 'hidden'; // Prevent scrolling while modal is open
     };
 
     const handleCloseModal = () => {
-        setSelectedImage(null);
+        setSelectedImageIndex(null);
         setLightboxDisplay(false);
         document.body.style.overflow = 'unset'; // Re-enable scrolling
     };
 
-    const imageDetails = {
-        '/nissanBefore.jpeg': 'Nissan Before Transformation',
-        '/hyundaiBefore.jpeg': 'Hyundai Before Transformation',
-        '/poloBefore.jpeg': 'Polo Before Transformation',
+    const handlePrevImage = () => {
+        setSelectedImageIndex((prevIndex) =>
+            prevIndex === 0 ? imageDetails.length - 1 : prevIndex - 1
+        );
     };
 
-    return (
-        <section id="gallery" className="bg-gray-100 text-center py-10 md:py-20 h-auto md:h-screen">
-            <h2 className="text-4xl md:text-6xl font-serif mb-6 md:mb-8 text-gray-900 tracking-wide leading-relaxed">
-                Gallery
-            </h2>
+    const handleNextImage = () => {
+        setSelectedImageIndex((prevIndex) =>
+            prevIndex === imageDetails.length - 1 ? 0 : prevIndex + 1
+        );
+    };
 
-            <div className="flex flex-wrap justify-center gap-4">
-                {Object.entries(imageDetails).map(([src, alt]) => (
-                    <div key={src} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-                        <div onClick={() => handleOpenModal(src.replace('Before', 'After'))} className="cursor-pointer overflow-hidden shadow-lg rounded-lg">
-                            <Image src={src} alt={alt} width={500} height={500} layout="responsive" className="transform transition duration-500 hover:scale-110" />
-                        </div>
-                        <span className="text-sm font-semibold">{alt}</span>
+
+    const imageDetails = [
+        { src: '/nissanBefore.jpeg', alt: 'Nissan Before Transformation' },
+        { src: '/nissanAfter.jpeg', alt: 'Nissan After Transformation' },
+        { src: '/hyundaiBefore.jpeg', alt: 'Hyundai Before Transformation' },
+        { src: '/hyundaiAfter.jpeg', alt: 'Hyundai After Transformation' },
+        { src: '/poloBefore.jpeg', alt: 'Polo Before Transformation' },
+        { src: '/poloAfter.jpeg', alt: 'Polo After Transformation' },
+        { src: '/img7.jfif',},
+        { src: '/img5.jfif',},
+        { src: '/img1.jfif',},
+        { src: '/img9.jfif', },
+        { src: '/img11.jfif'},
+        { src: '/img2.jfif'},
+        { src: '/img6.jfif'},
+        { src: '/img17.jfif'},
+        { src: '/img12.jfif'},
+        { src: '/img14.jfif'},
+        { src: '/img3.jfif'},
+        { src: '/img18.jfif'},
+        { src: '/img19.jfif'},
+        { src: '/img4.jfif'},
+        { src: '/img13.jfif'},
+        { src: '/img15.jfif'},
+        { src: '/img23.jfif'},
+        { src: '/img20.jfif'},
+        { src: '/img21.jfif'},
+        { src: '/img22.jfif'},
+        { src: '/img8.jfif'},
+        { src: '/img16.jfif'},
+        { src: '/img10.jfif'},
+    ];
+
+    return (
+        <section id="gallery" className="bg-gray-50 text-center py-16 md:py-24">
+            <h2 className="text-5xl font-bold mb-10 text-gray-900">
+                Our Work
+            </h2>
+            <p className="text-lg mb-12 text-gray-600">See the transformations we’ve made on various vehicles through our panel beating and bodywork services.</p>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 md:px-12">
+                {imageDetails.map(({ src, alt }, index) => (
+                    <div key={index} className="group relative cursor-pointer rounded-lg overflow-hidden shadow-lg">
+                        <div onClick={() => handleOpenModal(index)} className="relative h-64">
+                            <Image src={src} alt={alt} fill objectFit="cover" className="transform transition-transform duration-300 group-hover:scale-110" />                        </div>
+                        {/*<div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">*/}
+                        {/*    <span className="text-lg font-semibold">{alt}</span>*/}
+                        {/*</div>*/}
                     </div>
                 ))}
             </div>
-            {lightboxDisplay && (
-                <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-75">
-                    <div className="relative w-full max-w-3xl p-5 bg-white rounded-lg">
-                        <button onClick={handleCloseModal} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" aria-label="Close">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
+
+            {lightboxDisplay && selectedImageIndex !== null && (
+                <div
+                    className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-75 p-4"
+                    onClick={handleCloseModal} // Clicking outside the image closes the modal
+                >
+                    <div className="relative flex justify-center items-center max-w-4xl w-full"
+                         onClick={(e) => e.stopPropagation()}>
+                        {/* Left arrow for previous image */}
+                        <button
+                            onClick={handlePrevImage}
+                            className="absolute left-0 md:left-4 top-1/2 transform -translate-y-1/2 text-black bg-white rounded-full p-3 hov transition-colors z-10"
+                            aria-label="Previous Image"
+                        >
+                            &larr;
                         </button>
-                        <Image src={selectedImage} alt="Selected Image" width={600} height={600} layout="responsive" />
+
+                        {/* Image */}
+                        <div className="relative">
+                            <Image
+                                src={imageDetails[selectedImageIndex].src}
+                                alt={imageDetails[selectedImageIndex].alt}
+                                width={800}
+                                height={600}
+                                className="rounded-lg"
+                            />
+                        </div>
+
+                        {/* Right arrow for next image */}
+                        <button
+                            onClick={handleNextImage}
+                            className="absolute right-0 md:right-4 top-1/2 transform -translate-y-1/2 text-black bg-white rounded-full p-3 hover:bg-gray-900 transition-colors z-10"
+                            aria-label="Next Image"
+                        >
+                            &rarr;
+                        </button>
+
+                        {/* Close button */}
+                        <button
+                            onClick={handleCloseModal}
+                            className="absolute top-4 right-4 text-black bg-white rounded-full p-2 hover:bg-gray-900 transition-colors z-10"
+                            aria-label="Close"
+                        >
+                            &#10005;
+                        </button>
                     </div>
                 </div>
+
             )}
         </section>
     );
